53d84173819b37f1b72b00cf49d6981a1e23c710
Ami54321 <aparna.gharame136@gmail.com>
2026-08-01 14:06:35 +0530

    supabase.js

--- css/js/supabase.js
+++ css/js/supabase.js
@@ -1,1 +1,7 @@
+// ===============================
+// Believable HRMS - Supabase Config
+// ===============================
 
+const SUPABASE_URL = "https://fkajpmudsjidmtwlvdfn.supabase.co";
+
+const SUPABASE_ANON_KEY = "https://fkajpmudsjidmtwlvdfn.supabase.co/rest/v1/";
