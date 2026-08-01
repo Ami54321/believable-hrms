a55276a51453b13e43133469f807f8289ca36062
Ami54321 <aparna.gharame136@gmail.com>
2026-08-01 14:40:38 +0530

    app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,12 +1,3 @@
 function login() {
-
-    const employeeId = document.getElementById("employeeId").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    if (employeeId === "ADMIN" && password === "admin123") {
-        window.location.href = "admin.html";
-    } else {
-        alert("Invalid Employee ID or Password");
-    }
-
+    alert("Login button clicked!");
 }
