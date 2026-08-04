3512a331114b2671d9a5a16297eb0d0c8f2f0562
Ami54321 <aparna.gharame136@gmail.com>
2026-08-04 18:35:48 +0530

    A

--- js/employee.js
+++ js/employee.js
@@ -1,43 +1,43 @@
 alert("employee.js loaded");
+
 function addEmployee() {
 
     let empid = document.getElementById("empid").value;
     let fullname = document.getElementById("fullname").value;
     let status = document.getElementById("status").value;
 
-    if(empid=="" || fullname==""){
+    if (empid === "" || fullname === "") {
         alert("Please fill all fields");
         return;
     }
 
     let table = document.getElementById("employeeTable");
 
-    if(table.innerHTML.includes("No Employees Added Yet")){
-    table.innerHTML = "";
-}
-
-table.innerHTML += `
-<tr>
-    <td>${empid}</td>
-    <td>${fullname}</td>
-    <td>${status}</td>
-    <td>
-        <button onclick="deleteEmployee(this)">Delete</button>
-    </td>
-</tr>
-`;
-
-    document.getElementById("empid").value="";
-    document.getElementById("fullname").value="";
-    document.getElementById("password").value="";
-    document.getElementById("status").value="Active";
-}
-function deleteEmployee(button){
-
-    if(confirm("Are you sure you want to delete this employee?")){
-
-        button.parentElement.parentElement.remove();
-
+    if (table.innerHTML.includes("No Employees Added Yet")) {
+        table.innerHTML = "";
     }
 
+    let row = `
+    <tr>
+        <td>${empid}</td>
+        <td>${fullname}</td>
+        <td>${status}</td>
+        <td>
+            <button onclick="deleteEmployee(this)">Delete</button>
+        </td>
+    </tr>
+    `;
+
+    table.innerHTML += row;
+
+    document.getElementById("empid").value = "";
+    document.getElementById("fullname").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("status").value = "Active";
+}
+
+function deleteEmployee(button) {
+    if (confirm("Are you sure you want to delete this employee?")) {
+        button.closest("tr").remove();
+    }
 }
